3e34d6c42d582086fb68d98e1bc5f581dce7173b
Jimmy Alvarez <jimmyalvarezjr@gmail.com>
2021-02-04 14:36:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   if position_taken? == true
-    true
+    return true
   else position_taken? == false
-    false
+    return false
 end
 
 
