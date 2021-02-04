d455044744880476191fa464dfcbd127869fd926
Jimmy Alvarez <jimmyalvarezjr@gmail.com>
2021-02-04 14:50:28 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ def valid_move?(board, index)
     return true
   elsif index.between?(0,8) || position_taken?(board, index)
     return true
-  else index.between?(0,8) ! position_taken?(board,index)
+  else index.between?(0,8) !position_taken?(board,index)
     return false
   end
 end
